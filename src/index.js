83c89d8ebfb62fca5e42b5b0aaa00f0d10ad8d29
KorotkovK <korotkov.kostya123@gmail.com>
2023-11-05 14:48:31 +0200

    main

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
-import './components/styles.css';
+import './styles.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
